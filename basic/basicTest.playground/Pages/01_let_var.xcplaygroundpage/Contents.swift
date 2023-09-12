e0752edaabd4ac547a24ea092967ccc815911a2a
jooYeon <conn0204@naver.com>
2023-09-12 14:49:16 +0900

    01_let_var

---
+++ basic/basicTest.playground/Pages/01_let_var.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// 1. 선언과 초기화
+// 선언
+var rate: Double
+
+// 초기화 : 대입연산자(=)를 사용하여 값을 지정해주는 것
+rate = 4.0
+
+
+// 2. 상수와 변수
+// 상수 : 초기화 후 값을 바꿀 수 없다
+let nickName: String = "방울토마토"
+
+// 변수 : 초기화 후 값을 바꿀 수 있다
+var email: String = "testEmail@email.com"
+
+
+// 3. 타입 지정(어노테이션) 과 타입 추정
+//opt 누르고 변수 확인 -> 물음표 마크 뜨면서 변수 타입 확인 가능
+
+// 타입 지정
+var age: Int = 30
+
+// 타입 추정
+var gender = false
+
+
+
+// 4. 값 변경
+
+//nickName = "그냥토마토"
+//->let이라서 오류
+
+email = "test_test@emil.com"
